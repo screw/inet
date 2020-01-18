6f680f647098dfefa1d23efd78ce0efc9f1a5e7c
Marcel Marek <marcelma@ifi.uio.no>
2020-01-18 17:49:17 +0100

    Dmpr: Renamed inUseCong variable and signal to downstreamCongLevel

--- src/inet/routing/ospfv2/router/Lsa.h
+++ src/inet/routing/ospfv2/router/Lsa.h
@@ -46,7 +46,7 @@ struct NextHop
 
     double congLevel = 0;
     double fwdCongLevel = 0; //forwarded Congestion level
-    double inUseCongLevel = 0;
+    double downstreamCongLevel = 0;
     int packetCount = 0;
 
     int fwdPacketCount = 0; //total number of ECN capable packet forwarded within current interval
@@ -58,7 +58,7 @@ struct NextHop
 
     simsignal_t signalCongLevel = 0;
     simsignal_t signalfwdCongLevel = 0;
-    simsignal_t signalInUseCongLevel = 0;
+    simsignal_t signalDownstreamCongLevel = 0;
     simsignal_t signalFwdPacketCount = 0;
 
 };
